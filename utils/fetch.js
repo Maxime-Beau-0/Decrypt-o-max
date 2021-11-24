56556cbe367ba3e35caf8c0b8f6f8420b503e690
urbamaxime <beau.maxime@urbasolar.com>
2021-11-24 20:22:26 +0100

    add: 5 minutes cache for the list of coins

--- utils/fetch.js
+++ utils/fetch.js
@@ -1,6 +1,9 @@
+const CACHE_DURATION = 5 * 60 * 1000; // 5 minutes
+
 const fetchTopic = async (topic, options) => {
   // Get options
-  const forceRefresh = true; // options && options.forceRefresh;
+  const ttl = await getFromStorage(`${topic}_ttl`);
+  const forceRefresh = options && options.forceRefresh || (!ttl || ttl <  Date.now() - CACHE_DURATION);
   const data = options && options.data ? options.data : {};
 
   const topicInStorage = await getFromStorage(topic);
@@ -10,11 +13,13 @@ const fetchTopic = async (topic, options) => {
       console.info("Extension : Fetched from cache - ", topicInStorage);
       return resolve(topicInStorage);
     }
+    console.info("Extension : Fetching topic, not using cache - ", topic);
     chrome.runtime.sendMessage({ topic, data }, (messageResponse) => {
       if (messageResponse === null || messageResponse === undefined) {
         reject(new Error(`Error fetching topic - ${topic}`));
       }
       setInStorage(topic, messageResponse);
+      setInStorage(`${topic}_ttl`, Date.now());
       return resolve(messageResponse);
     });
   });
