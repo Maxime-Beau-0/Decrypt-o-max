b60670c999e99b6798e59955fc226b4c630fc39d
urbamaxime <beau.maxime@urbasolar.com>
2021-11-24 20:00:23 +0100

    modif: explorer logo from fontawesome to svg file Modification made to avoid content security policy's problems fix: problems with empty symbols

--- symbols/symbols.js
+++ symbols/symbols.js
@@ -1,7 +1,7 @@
 (async () => {
   // Get all coins & create regexp from it
   const coins = (await fetchTopic("coins")).filter(
-    (coin) => !coin.id.startsWith("binance-peg")
+    (coin) => coin.symbol !== '' && coin.name !== '' && !coin.id.startsWith("binance-peg")
   );
   const regexps = [];
   // List of names then list of symbols
@@ -181,6 +181,7 @@
       
       // Get all eth addresses in this node's text
       const ethMatches = Array.from(node.textContent.matchAll(regexpEth));
+      console.log('ethMatches', ethMatches, node.textContent, regexpEth);
       // Iterate in reverse order (terms at the end of the node first)
       // because we're going to split the current node on each iteration, and only keep the beginning
       for (const match of ethMatches.sort((match1, match2) =>
@@ -346,38 +347,69 @@
       coinInformations.links.homepage.length > 0
         ? coinInformations.links.homepage[0]
         : null;
-    if (homepage)
+    if (homepage) {
       Boundary.find("#dcmax_link_homepage").attr("href", homepage).show();
+      Boundary.find("#dcmax_logo_homepage")
+        .attr("src", chrome.runtime.getURL("images/icons/laptop-house.svg"))
+        .show();
+    }
     else Boundary.find("#dcmax_link_homepage").hide();
+
     const subreddit = coinInformations.links.subreddit_url || null;
-    if (subreddit)
+    if (subreddit) {
       Boundary.find("#dcmax_link_reddit").attr("href", subreddit).show();
+      Boundary.find("#dcmax_logo_reddit")
+        .attr("src", chrome.runtime.getURL("images/icons/reddit-alien.svg"))
+        .show();
+    }
     else Boundary.find("#dcmax_link_reddit").hide();
+
     const twitterHandle = coinInformations.links.twitter_screen_name || null;
-    if (twitterHandle)
+    if (twitterHandle) {
       Boundary.find("#dcmax_link_twitter")
         .attr("href", "https://twitter.com/" + twitterHandle)
         .show();
+      Boundary.find("#dcmax_logo_twitter")
+        .attr("src", chrome.runtime.getURL("images/icons/twitter.svg"))
+        .show();
+    }
     else Boundary.find("#dcmax_link_twitter").hide();
+
     const facebookUsername = coinInformations.links.facebook_username || null;
-    if (facebookUsername)
+    if (facebookUsername) {
       Boundary.find("#dcmax_link_facebook")
         .attr("href", "https://facebook.com/" + facebookUsername)
         .show();
+      Boundary.find("#dcmax_logo_facebook")
+        .attr("src", chrome.runtime.getURL("images/icons/facebook-f.svg"))
+        .show();
+    }
     else Boundary.find("#dcmax_link_facebook").hide();
+
     const telegramIdentifier =
       coinInformations.links.telegram_channel_identifier || null;
-    if (telegramIdentifier)
+    if (telegramIdentifier) {
       Boundary.find("#dcmax_link_telegram")
         .attr("href", "https://t.me/" + telegramIdentifier)
         .show();
+      Boundary.find("#dcmax_logo_telegram")
+        .attr("src", chrome.runtime.getURL("images/icons/telegram-plane.svg"))
+        .show();
+    }
     else Boundary.find("#dcmax_link_telegram").hide();
+
     const github =
       coinInformations.links.repos_url.github.length > 0
         ? coinInformations.links.repos_url.github[0]
         : null;
-    if (github) Boundary.find("#dcmax_link_github").attr("href", github).show();
+    if (github) {
+      Boundary.find("#dcmax_link_github").attr("href", github).show();
+      Boundary.find("#dcmax_logo_github")
+        .attr("src", chrome.runtime.getURL("images/icons/github.svg"))
+        .show();
+    }
     else Boundary.find("#dcmax_link_github").hide();
+
     Boundary.find("#dcmax_link_coinmarketcap")
       .attr(
         "href",
@@ -388,7 +420,7 @@
       .attr("href", "https://www.coingecko.com/en/coins/" + coinInformations.id)
       .show();
     Boundary.find("#dcmax_logo_coingecko")
-      .attr("src", chrome.runtime.getURL("images/coingecko_logo.png"))
+      .attr("src", chrome.runtime.getURL("images/icons/coingecko_logo.png"))
       .show();
     const explorer =
       coinInformations.links.blockchain_site.length > 0
@@ -397,8 +429,12 @@
               link.includes("etherscan.io") || link.includes("bscscan.com")
           ) || coinInformations.links.blockchain_site[0]
         : null;
-    if (explorer)
+    if (explorer) {
       Boundary.find("#dcmax_link_explorer").attr("href", explorer).show();
+      Boundary.find("#dcmax_logo_explorer")
+        .attr("src", chrome.runtime.getURL("images/icons/list-alt.svg"))
+        .show();
+    }
     else Boundary.find("#dcmax_link_explorer").hide();
     // Logo & images
     if (coinInformations.image.small) {
@@ -440,8 +476,8 @@
       `${formatToUsNumber(addressInformations.bscBalance * 0.000000000000000001, 6)} BNB`
     );
     // Links, logos & social media
-    Boundary.find("#dcmax_logo_eth").attr("src", chrome.runtime.getURL("images/eth-diamond-purple.png")).show();
-    Boundary.find("#dcmax_logo_bnb").attr("src", chrome.runtime.getURL("images/binance-coin-bnb-logo.png")).show();
+    Boundary.find("#dcmax_logo_eth").attr("src", chrome.runtime.getURL("images/icons/eth-diamond-purple.png")).show();
+    Boundary.find("#dcmax_logo_bnb").attr("src", chrome.runtime.getURL("images/icons/binance-coin-bnb-logo.png")).show();
     Boundary.find("#dcmax_link_etherscan").attr("href", `https://etherscan.io/address/${address}`).show();
     Boundary.find("#dcmax_link_bscscan").attr("href", `https://bscscan.com/address/${address}`).show();
     return true;
